8faee9dac96d0a9a3ccf2e19f67d7df5fd482b39
sanpawat <koyong555@hotmail.com>
2025-11-15 20:08:19 +0700

    Initial Commit

---
+++ cha-thai/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cha-thai
+//
+//  Created by Sanpawat Sewsuwan on 15/11/2568 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cha-thai/cha_thaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  cha_thaiApp.swift
+//  cha-thai
+//
+//  Created by Sanpawat Sewsuwan on 15/11/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct cha_thaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
